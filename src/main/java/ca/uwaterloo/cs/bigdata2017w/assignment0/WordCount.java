f3e58a72a381c0fd90e60352343d6e851c21ff85
Hanxin Zhang <William@v1020-wn-176-150.campus-dynamic.uwaterloo.ca>
2017-01-05 09:32:36 -0500

    add data;correct pom.xml;correct packge name

--- src/main/java/ca/uwaterloo/cs/bigdata2017w/assignment0/WordCount.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2017w/assignment0/WordCount.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.bespin.java.mapreduce.wordcount;
+package ca.uwaterloo.cs.bigdata2017w.assignment0;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
